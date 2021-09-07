a426ee772ba50da9b50feb6f442522039e6ebc41
Karim Daou <kdaou@e1r2p12.1337.ma>
2021-09-07 15:32:17 +0100

    set up mongodb connection ; add user middleware/ router

--- api/app.ts
+++ api/app.ts
@@ -1,8 +1,20 @@
+require('dotenv').config()
+
 import express, { Request, Response, NextFunction } from 'express'
+import mongoose from 'mongoose';
 import errorMiddleware from './middlewares/error.middleware';
 import HttpException from './exceptions/HttpException';
+import userRouter from './routes/user.routes'
+
+mongoose.connect(process.env.DB_URL || 'mongodb://127.0.0.1:27017/blog',
+    () => console.log('MongoDB connected successfully.'))
 
 const app = express();
+
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json());
+
+app.use('/user', userRouter)
 
 app.use((request: Request, response: Response, next: NextFunction) => {
     const error = new HttpException(404, 'Not found');
---
+++ api/routes/user.routes.ts
@@ -0,0 +1,9 @@
+import express from 'express'
+// import userMiddleware from '../middlewares/user.middleware'
+// import userController from '../controllers/user.controller'
+
+const router = express.Router();
+
+// router.post('/create', userMiddleware.createUserValidator, userController.createUser);
+
+export default router;
