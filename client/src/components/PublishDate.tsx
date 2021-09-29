8f5fafe2fe47035d393a589c61ea66722905a658
menoiy <karim.daou.1993@gmail.com>
2021-09-29 09:11:18 +0100

    remove useless div in Publishdate component

--- client/src/components/PublishDate.tsx
+++ client/src/components/PublishDate.tsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 import { formatDistanceToNowStrict } from "date-fns";
 
-const PublishDate = (props: { date: Date }) => {
+const PublishDate = (props: { date: Date }): JSX.Element => {
   //
   const getElapsedTime = () => {
     return formatDistanceToNowStrict(new Date(props.date), {
@@ -12,12 +12,9 @@ const PublishDate = (props: { date: Date }) => {
   return <Container>{getElapsedTime()}</Container>;
 };
 
-const Container = styled.div`
+const Container = styled.p`
   font-size: 90%;
   color: rgba(100, 100, 100, 0.5);
-  display: flex;
-  justify-content: center;
-  align-items: center;
 `;
 
 export default PublishDate;
