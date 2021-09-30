161d7eef0a80d9ebfce1ec0e5a30cb0ffa4c2558
menoiy <karim.daou.1993@gmail.com>
2021-09-30 14:22:27 +0100

    create custom exceptions

---
+++ server/api/exceptions/index.ts
@@ -0,0 +1,63 @@
+export class HttpException extends Error {
+  public status: number;
+  public message: string;
+  constructor(status: number, message: string) {
+    super(message);
+    this.status = status;
+    this.message = message;
+  }
+}
+
+export class NotAuthorizedException extends HttpException {
+  constructor() {
+    super(401, "You're not authorized");
+  }
+}
+
+export class UserNotFound extends HttpException {
+  constructor() {
+    super(404, 'User not found');
+  }
+}
+
+export class EmailExist extends HttpException {
+  constructor() {
+    super(409, 'This email address is already being used');
+  }
+}
+
+export class UsernameExist extends HttpException {
+  constructor() {
+    super(409, 'This username is already being used');
+  }
+}
+
+export class WrongPassword extends HttpException {
+  constructor() {
+    super(401, "The password that you've entered is incorrect");
+  }
+}
+
+export class WrongUsername extends HttpException {
+  constructor() {
+    super(401, "Username you entered isn't connected to an account");
+  }
+}
+
+export class EmailNotVerified extends HttpException {
+  constructor() {
+    super(401, 'Account not verified, verify your email address');
+  }
+}
+
+export class PostNotFound extends HttpException {
+  constructor() {
+    super(404, 'Post not found');
+  }
+}
+
+export class CommentNotFound extends HttpException {
+  constructor() {
+    super(404, 'Comment not found');
+  }
+}
--- server/api/middlewares/error.middleware.ts
+++ server/api/middlewares/error.middleware.ts
@@ -1,14 +1,11 @@
-import { HttpException } from '../exceptions/HttpException';
 import { Request, Response, NextFunction } from 'express';
+import { HttpException } from '../exceptions/';
 
-const errorMiddleware = (error: HttpException, request: Request, response: Response, next: NextFunction) => {
+export const errorMiddleware = (error: HttpException, request: Request, response: Response, next: NextFunction) => {
   const status = error.status || 500;
   const message = error.message || 'something wrong.';
-  response.status(status).json({
-    error: {
-      message
-    }
+  response.status(status).send({
+    status,
+    message
   });
 };
-
-export default errorMiddleware;
