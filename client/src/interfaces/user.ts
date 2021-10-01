42c852ee6e830e167df898f44050983f432feb43
menoiy <karim.daou.1993@gmail.com>
2021-10-01 15:51:33 +0100

    remove no used file

--- client/src/interfaces/user.ts
+++
@@ -1,9 +0,0 @@
-import { HexBase64BinaryEncoding } from "crypto";
-
-export interface IUser {
-  _id: HexBase64BinaryEncoding;
-  username: string;
-  firstName: string;
-  lastName: string;
-  email: string;
-}
