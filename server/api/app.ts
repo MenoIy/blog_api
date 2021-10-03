903e16d1f28abdf99a0785f4c525bc8786c4d317
menoiy <karim.daou.1993@gmail.com>
2021-10-03 19:20:32 +0100

    make uploads folder static

--- server/api/app.ts
+++ server/api/app.ts
@@ -26,6 +26,7 @@ app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cookieParser());
+app.use('/api/v1/uploads', express.static('uploads'));
 
 app.use('/api/v1/setup', routes.setup);
 app.use('/api/v1/users', routes.userRoutes);
