0b4c1be04264884e0da7fbc744b011a94bd14c93
menoiy <karim.daou.1993@gmail.com>
2021-10-08 11:46:08 +0100

    make comment and post getters public

--- server/api/routes/comment.routes.ts
+++ server/api/routes/comment.routes.ts
@@ -7,7 +7,7 @@ import { authenticated } from '../middlewares/auth';
 export const router: Router = express.Router();
 
 router.post('/:postId/comments', authenticated, commentMiddleware.createComment, commentController.createComment);
-router.get('/:postId/comments', authenticated, commentMiddleware.getComments, commentController.getComments);
-router.get('/:commentId', authenticated, commentMiddleware.getComment, commentController.getComment);
+router.get('/:postId/comments', commentMiddleware.getComments, commentController.getComments);
+router.get('/:commentId', commentMiddleware.getComment, commentController.getComment);
 router.patch('/:commentId', authenticated, commentMiddleware.updateComment, commentController.updateComment);
 router.delete('/:commentId', authenticated, commentController.deleteComment, commentController.deleteComment);
