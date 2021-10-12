35712bd93f62bfc6d198e6d20da1f182195f49bd
menoiy <karim.daou.1993@gmail.com>
2021-10-12 20:47:56 +0100

    update validator order

--- client/src/validators/index.ts
+++ client/src/validators/index.ts
@@ -7,24 +7,24 @@ export const registerSchema = yup.object({
   email: yup.string().email().required(),
   password: yup
     .string()
-    .min(8, "Minimum 8 characters")
-    .max(24, "Maximum 24 characters")
-    .required("Password is required")
     .matches(/^(?=.*?[A-Z])/, "Must contain at least one upper case")
     .matches(/^(?=.*?[0-9])/, "Must contain at least one digit")
-    .matches(/^(?=.*?[a-z])/, "Must contain at least one lower case"),
+    .matches(/^(?=.*?[a-z])/, "Must contain at least one lower case")
+    .min(8, "Minimum 8 characters")
+    .max(24, "Maximum 24 characters")
+    .required("Password is required"),
 });
 
 export const loginSchema = yup.object({
   username: yup.string().min(6).max(24).required(),
   password: yup
     .string()
-    .min(8, "Minimum 8 characters")
-    .max(24, "Maximum 24 characters")
-    .required("Password is required")
     .matches(/^(?=.*?[A-Z])/, "Must contain at least one upper case")
     .matches(/^(?=.*?[0-9])/, "Must contain at least one digit")
-    .matches(/^(?=.*?[a-z])/, "Must contain at least one lower case"),
+    .matches(/^(?=.*?[a-z])/, "Must contain at least one lower case")
+    .min(8, "Minimum 8 characters")
+    .max(24, "Maximum 24 characters")
+    .required("Password is required"),
 });
 
 export const commentSchema = yup.object({
